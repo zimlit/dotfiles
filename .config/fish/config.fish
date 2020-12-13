7884f3ad418eacf8c239ca5c393b97321f71603b
zimlit <zimlit2010@gmail.com>
2020-12-13 09:25:36 -0500

    improve prompt char

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,13 @@
 function fish_prompt
-    echo -n (set_color 00a06f)$USER(set_color normal)@(set_color 006fa0)(prompt_hostname)
+    set pc "\$ "
+    if test (whoami) = "root"
+        set pc "# "
+    end
+    echo -n (set_color 00a06f)(whoami)(set_color normal)@(set_color 006fa0)(prompt_hostname)
     set_color 00a000
     echo -n \ (prompt_pwd)
     set_color a08000
     echo -n (fish_git_prompt)
     set_color normal
-    echo \ "\$ "
+    echo \ $pc
 end
